6a3ea00a105b3822e4259c68ce4d2774e84d2b43
Sergio <52042600+SergioFC1989@users.noreply.github.com>
2023-07-01 20:27:07 +0200

    [API]: feat: Modificaciones en las configuraciones
    Co-authored-by: SergioFCU <sergiofcu@ext.inditex.com>

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const express = require("express");
+const compression = require("compression");
 const cors = require("cors");
-
+const express = require("express");
+const helmet = require("helmet");
 const { API } = require("./api/api.js");
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 3000;
 const app = express();
+
+// middleware
+app.use(express.static("public"));
+app.use(compression());
 app.use(cors());
+app.use(helmet());
 
 API.forEach((elem) => {
-  app
-    .route(elem.route)
-    [elem.method.toLowerCase()](async (req, res) => elem.function(req, res));
+  app.route(elem.route)[elem.method.toLowerCase()](async (req, res) => {
+    try {
+      await elem.function(req, res);
+    } catch (error) {
+      res.send(error);
+    }
+  });
 });
 
 app.listen(port, () => {
